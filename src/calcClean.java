545d8424562448a9d5ff908753a5eb66fd6cc4e8
bteodosio <teodosio.goncalves@gmail.com>
2020-08-24 16:28:06 -0300

    Primeira Refatoracao: Acerto dos nomes das variaveis

--- src/calcClean.java
+++ src/calcClean.java
@@ -5,76 +5,70 @@ public class calcClean {
     
 
     public static void main(String[] args){
-        BufferedReader ent = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader entradaConsole = new BufferedReader(new InputStreamReader(System.in));
         try{
-            Boolean noErro = true;
-            String valorDigitado = "";
-            Double fator1 = 0.0;
-            Double fator2 = 0.0;
-            Double resultado = 0.0;
-            String operacao = "";
+            Boolean valorIncorreto = true;
+            Double fatorOperacao1 = 0.0;
+            Double fatorOperacao2 = 0.0;
+            Double resultadoOperacao = 0.0;
+            String identificadorOperacao = "";
 
-            while(noErro){
+            while(valorIncorreto){
                 System.out.println("Digite o primeiro numero:\n");
-                valorDigitado = ent.readLine();
                 try{
-                    fator1 = Double.parseDouble(valorDigitado);
-                    noErro = false;
+                    fatorOperacao1 = Double.parseDouble(entradaConsole.readLine());
+                    valorIncorreto = false;
                 } catch (NumberFormatException err){
                     System.out.println("Numero Invalido, tente novamente!");
                 }
             }
 
-            noErro = true;
-            while(noErro){                
-                System.out.println("Digite o segundo numero:\n");
-                valorDigitado = ent.readLine();
-                
-                try{
-                    fator2 = Double.parseDouble(valorDigitado);
-                    noErro = false;
-                } catch (NumberFormatException err){
-                    System.out.println("Numero Invalido, tente novamente!");
-                }
-            }
-
-            noErro = true;
-            while(noErro){                
-                System.out.println("Digite a operacao [+,-,/,*]\n");
-                valorDigitado = ent.readLine();
-                operacao = valorDigitado;
-               // if(((valorDigitado != "/") && (valorDigitado != "*") && (valorDigitado != "-") && (valorDigitado != "+")) == true){
-                if(!operacao.equals("+") && !operacao.equals("*") && !operacao.equals("/") && !operacao.equals("-")){
+            valorIncorreto = true;
+            while(valorIncorreto){                
+                System.out.println("Digite a identificadorOperacao [+,-,/,*]\n");
+                identificadorOperacao = entradaConsole.readLine();
+                if(!identificadorOperacao.equals("+") && !identificadorOperacao.equals("*") && !identificadorOperacao.equals("/") && !identificadorOperacao.equals("-")){
                     System.out.println("Operacao Invalida, tente novamente!");
                 }else{
-                    noErro = false;
+                    valorIncorreto = false;
                 }
                 
             }
 
-            switch (operacao) {
+            valorIncorreto = true;
+            while(valorIncorreto){                
+                System.out.println("Digite o segundo numero:\n");
+                try{
+                    fatorOperacao2 = Double.parseDouble(entradaConsole.readLine());
+                    valorIncorreto = false;
+                } catch (NumberFormatException err){
+                    System.out.println("Numero Invalido, tente novamente!");
+                }
+            }
+
+            switch (identificadorOperacao) {
                 case "/":
-                    System.out.println("Dividindo " + fator1 + " por " + fator2 + "\n");
-                    resultado = fator1 / fator2;
-                    System.out.println("Resultado " + resultado + "\n");
+                    System.out.println("Dividindo " + fatorOperacao1 + " por " + fatorOperacao2 + "\n");
+                    resultadoOperacao = fatorOperacao1 / fatorOperacao2;
+                    System.out.println("Resultado " + resultadoOperacao + "\n");
                     break;
                 
                 case "*":
-                    System.out.println("Multiplicando " + fator1 + " por " + fator2 + "\n");
-                    resultado = fator1 * fator2;
-                    System.out.println("Resultado " + resultado + "\n");
+                    System.out.println("Multiplicando " + fatorOperacao1 + " por " + fatorOperacao2 + "\n");
+                    resultadoOperacao = fatorOperacao1 * fatorOperacao2;
+                    System.out.println("Resultado " + resultadoOperacao + "\n");
                     break;
 
                 case "-":
-                    System.out.println("Subritraindo " + fator2 + " de " + fator1 + "\n");
-                    resultado = fator1 - fator2;
-                    System.out.println("Resultado " + resultado + "\n");
+                    System.out.println("Subritraindo " + fatorOperacao2 + " de " + fatorOperacao1 + "\n");
+                    resultadoOperacao = fatorOperacao1 - fatorOperacao2;
+                    System.out.println("Resultado " + resultadoOperacao + "\n");
                     break;
                 
                 case "+":
-                    System.out.println("Somando " + fator2 + " a " + fator1 + "\n");
-                    resultado = fator1 + fator2;
-                    System.out.println("Resultado " + resultado + "\n");
+                    System.out.println("Somando " + fatorOperacao2 + " a " + fatorOperacao1 + "\n");
+                    resultadoOperacao = fatorOperacao1 + fatorOperacao2;
+                    System.out.println("Resultado " + resultadoOperacao + "\n");
                         break;
 
                 default:
