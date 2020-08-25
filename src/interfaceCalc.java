7af1be6c389441a0156fe47c393d83274463c2e2
bteodosio <teodosio.goncalves@gmail.com>
2020-08-25 18:03:27 -0300

    Oitava Refatoracao: Removendo funcoes Desnecessarias

--- src/interfaceCalc.java
+++ src/interfaceCalc.java
@@ -5,24 +5,16 @@ import java.io.IOException;
 public class interfaceCalc {
     private BufferedReader entradaConsole = new BufferedReader(new InputStreamReader(System.in));
 
-    private Double receberDouble (){
-
-        Double valorDouble = 0.0;
+    public Double receberOperador(){
+        Double operador = 0.0;
+        
         try{
-            valorDouble = Double.parseDouble(entradaConsole.readLine().replace(',', '.'));
+            operador = Double.parseDouble(entradaConsole.readLine().replace(',', '.'));
         } catch (NumberFormatException err){
             return null;
         } catch (IOException err){
             return null;
-        }
-
-        return valorDouble;
-    }
-
-    public Double receberOperador(){
-        Double operador = 0.0;
-        
-        operador = receberDouble();
+        };
 
         return operador;
     }
